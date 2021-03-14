9dbd0891bcc60dfeda990b618d2dae0b66762c54
raffy <raffy@localhost>
2021-03-14 11:47:04 -0700

    added reverse string

---
+++ Java_Tachnical_Interview_Prep/src/most_asked/ReverseString.java
@@ -0,0 +1,20 @@
+package most_asked;
+
+public class ReverseString {
+	// write a function to reverse a string
+	public static void main(String[] args) {
+
+		System.out.println(rev("Java"));
+
+	}
+
+	public static String rev(String str) {
+		String reversed = "";
+		for (int i = str.length()-1; i >= 0; i--) {
+			reversed += str.charAt(i);
+		}
+
+		return reversed;
+	}
+
+}
