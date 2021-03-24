a31a14a88936ac9cf0c05de0b75abf0ed97981e4
raffy <raffy@localhost>
2021-03-24 09:35:13 -0700

    Day05

---
+++ Java_Tachnical_Interview_Prep/src/most_asked/frequencyOfCharacter.java
@@ -0,0 +1,51 @@
+package most_asked;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class frequencyOfCharacter {
+   public static void main(String[] args) {
+	 /*
+	  Return the frequency of each character in the given string 
+		Ex:
+		Input: DDDAACCCCCCB Output: D3A2C6B1
+	  */
+	
+	  System.out.println(frequencyOfChar("DDDAACCCCCCB"));
+	   
+}
+   
+//   public static String frequencyOfChar(String str) {
+//	    Map<String,Integer> map=new HashMap<>();
+//	    for(String each : str.split("")) {
+//	    	if(map.containsKey(each)) {
+//	    		map.put(each, map.get((each)+1));
+//	    	}else {
+//	    		map.put(each, 1);
+//	    	}
+//	    		
+//	    }
+//	   
+//	   str="";
+//	   for(Map.Entry<String, Integer> each: map.entrySet()) {
+//		   str += each.getKey()+each.getValue();
+//	   }
+//	   
+//	   return str;
+	   
+ //  }
+   public static String frequencyOfChar(String str){ 
+	   Map<String, Integer> map = new HashMap<>();
+   for (String each : str.split("")) {
+   if (map.containsKey(each)) {
+	   map.put(each, map.get(each) + 1);
+   } else { map.put(each, 1);
+   } 
+   }
+   str = "";
+   for(Map.Entry<String, Integer> each: map.entrySet()){
+   str += each.getKey()+each.getValue(); }
+   return str; }
+   
+   
+}
